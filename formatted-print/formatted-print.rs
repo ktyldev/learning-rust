6e278ee57c3bd9efee5901b83d914f292516d621
Cat Flynn <me@monodokimes.uk>
2019-02-18 23:26:15 +0000

    add gitignore, more text formatting fun

--- formatted-print/formatted-print.rs
+++ formatted-print/formatted-print.rs
@@ -1,8 +1,17 @@
-#[derive(Debug)]
-struct DebugPrintable(i32);
+use std::fmt;
 
 #[derive(Debug)]
-struct Deep(DebugPrintable);
+struct Structure(i32);
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Deep(Structure);
+
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 fn main() {
     // println!("{} of {:b} people know binary, the other half doesn't", 1, 2);
@@ -13,9 +22,8 @@ fn main() {
     // let pi = 3.141592;
     // println!("{:.3}", pi);
 
-    println!("Now {:?} will print!", DebugPrintable(3));
-
-    println!("Now {:#?} will print!", Deep(DebugPrintable(4))); 
+    println!("Now {} will print!", Structure(3));
+    // println!("Now {:#?} will print!", Deep(Structure(4))); 
 }
 
 
